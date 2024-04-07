24dfb5b1959ae761870b2ed91bfec54daea2056b
jhlee <jhlee9462@gmail.com>
2024-04-07 18:05:44 +0900

    파이참으로 간단한 쟝고 앱 만들기

--- rest/urls.py
+++ rest/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('rest/', views.temp_here, name='temp_here'),
+    path('rest/discover', views.temp_somewhere, name='temp_somewhere'),
 ]
--- rest/views.py
+++ rest/views.py
@@ -4,9 +4,22 @@ from django.shortcuts import render
 import requests
 from django.template import loader
 
+from rest.models import Worldcities
+
 
 def temp_here(request):
     location = geocoder.ip('me').latlng
+    temp = get_temp(location)
+    template = loader.get_template('index.html')
+    context = {
+        'city': 'Your location',
+        'country': 'Your country',
+        'temp': temp
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def get_temp(location):
     endpoint = "https://api.open-meteo.com/v1/forecast"
     api_request = f"{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature_2m"
     from datetime import datetime
@@ -14,6 +27,19 @@ def temp_here(request):
     hour = now.hour
     meteo_data = requests.get(api_request).json()
     temp = meteo_data['hourly']['temperature_2m'][hour]
-    template = loader.get_template('index.html')
-    context = {'temp': temp}
+    return temp
+
+
+def temp_somewhere(request):
+    random_item = Worldcities.objects.all().order_by('?').first()
+    city = random_item.city
+    country = random_item.country
+    location = [random_item.lat, random_item.lng]
+    temp = get_temp(location)
+    template = loader.get_template("index.html")
+    context = {
+        'city': city,
+        'country': country,
+        'temp': temp
+    }
     return HttpResponse(template.render(context, request))
